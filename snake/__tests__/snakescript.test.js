c5594d7af98b85723275ea409a28bfa8ac28efc5
Rami <ramilejmi@gmail.com>
2018-04-18 00:27:05 +0300

    More fixing to match eslint rules

--- snake/__tests__/snakescript.test.js
+++ snake/__tests__/snakescript.test.js
@@ -7,36 +7,35 @@ test('class defined', () => {
 });
 
 test('d defined', () => {
-    expect(snakescript.d).toBeDefined();
-  });
+  expect(snakescript.d).toBeDefined();
+});
 
 test('color defined', () => {
-   expect(snakescript.color).toBeDefined();
- });
+  expect(snakescript.color).toBeDefined();
+});
 
- test('cw defined', () => {
-    expect(snakescript.cw).toBeDefined();
-  });
+test('cw defined', () => {
+  expect(snakescript.cw).toBeDefined();
+});
 
-  test('speed defined', () => {
-    expect(snakescript.speed).toBeDefined();
-  });
+test('speed defined', () => {
+  expect(snakescript.speed).toBeDefined();
+});
 
 
-
- //  Checking that values match
- test('d matches', () => {
-    expect(snakescript.d).toBe("right");
-  });
+//  Checking that values match
+test('d matches', () => {
+  expect(snakescript.d).toBe('right');
+});
 
 test('color matches', () => {
-   expect(snakescript.color).toBe("white");
- });
+  expect(snakescript.color).toBe('white');
+});
 
- test('cw matches', () => {
-    expect(snakescript.cw).toBe(15);
-  });
+test('cw matches', () => {
+  expect(snakescript.cw).toBe(15);
+});
 
-  test('speed matches', () => {
-    expect(snakescript.speed).toBe(130);
-  });
+test('speed matches', () => {
+  expect(snakescript.speed).toBe(130);
+});
